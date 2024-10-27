ec96e9dda85313b4812c7ab79da31eb78159e5db
Xanbert <xanbertx@outlook.com>
2024-10-27 19:18:03 +0800

    Misc: Startup cannot open file fixed up

--- tools/dashboard-init.el
+++ tools/dashboard-init.el
@@ -9,10 +9,16 @@
 
 (use-package dashboard
   :ensure t
+  :demand t
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-startup-banner 'logo)
-  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+  (setq dashboard-icon-type 'all-the-icons)  ; use `all-the-icons' package
+  (setq initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name)))
+  ;; Content is not centered by default. To center, set
+  (setq dashboard-center-content t)
+  ;; vertically center content
+  (setq dashboard-vertically-center-content t)
   ;:hook
   ;(after-make-frame-functions . dashboard-setup-startup-hook)
   )
